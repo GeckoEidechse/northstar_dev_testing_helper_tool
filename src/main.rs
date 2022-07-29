7590b174007f17321cd2be5ecf3787eeca5448f3
GeckoEidechse <gecko.eidechse+git@pm.me>
2022-07-29 14:45:40 +0200

    Remove more wasm related stuff

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-// When compiling natively:
-#[cfg(not(target_arch = "wasm32"))]
 fn main() {
     // Log to stdout (if you run with `RUST_LOG=debug`).
     tracing_subscriber::fmt::init();
