658686ecadbc5aba2a3e9089f6177761cc342676
GeckoEidechse <gecko.eidechse+git@pm.me>
2022-07-31 11:41:43 +0200

    Use correct name for binary

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ pub fn do_self_update() -> Result<(), Box<dyn ::std::error::Error>> {
     let status = self_update::backends::github::Update::configure()
         .repo_owner("GeckoEidechse")
         .repo_name("northstar_dev_testing_helper_tool")
-        .bin_name("github") // <-- name of the binary in the zip to use to replcae current version with
+        .bin_name("northstar_dev_testing_helper_tool_bin") // <-- name of the binary in the zip to use to replcae current version with
         .show_download_progress(true)
         .current_version(cargo_crate_version!())
         .build()?
